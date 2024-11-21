3f0eaec861687297697caaeb0533b0ee61727193
Coven Deity <covendeity@pursuitofhappiness.cloud>
2024-11-21 20:42:50 +0000

    Changed hello message

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -9,17 +9,18 @@ module.exports = {
         if (!interaction.author.bot) {
 
             const lowerCaseContent = interaction.content.toLowerCase();
+            const currentTime = new Date().toLocaleString();
 
-            const regEx = /^hello bot/;
+            const regEx = /^hello bot$/;
             if ((lowerCaseContent.match(regEx) !== null)) {
                 const embed = new discordEmbedBuilder()
-                    .setTitle('Hello Message')
-                    .setDescription(`Hello <@${interaction.author.id}>`)
+                    .setTitle('Hey there!')
+                    .setDescription(`Hey <@${interaction.author.id}>, glad you're here! What can I do for you today? `)
                     .setFooter({
                         text: `Drone`,
                         iconURL: `https://raw.githubusercontent.com/Coven-Deity/Drone/refs/heads/main/Drone_icon.jpg`
                     });
-                embed.addFields({ name: 'Field1', value: `Field1_Value` });
+                embed.addFields({ name: 'Current Time', value: currentTime });
                 return await interaction.channel.send({ embeds: [embed] });
             }
 
