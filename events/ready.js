42312dfb88b0fe0bd23801ab61c81ca32ac705fc
Coven Deity <covendeity@pursuitofhappiness.cloud>
2024-11-22 14:21:13 +0000

    Fixed bug in Repository check

--- events/ready.js
+++ events/ready.js
@@ -30,6 +30,8 @@ async function fetchRepoInfo(repoOwner, repoName) {
 }
 
 async function checkForRepoChanges(repoOwner, repoName) {
+	const now = new Date();
+	const isoString = now.toISOString();
 	const repoPreviousStateFile = 'repoPreviousState.json';
 	let repoPreviousState;
 
@@ -39,6 +41,7 @@ async function checkForRepoChanges(repoOwner, repoName) {
 	} catch (error) {
 		if (error.code === 'ENOENT') {
 			repoPreviousState = {
+				updated_at: isoString,
 				size: 0,
 				open_issues_count: 0
 			};
@@ -48,10 +51,12 @@ async function checkForRepoChanges(repoOwner, repoName) {
 		}
 	}
 	const repoInfo = await fetchRepoInfo(repoOwner, repoName);
+	const updated_at = repoInfo.updated_at;
 	const size = repoInfo.size;
 	const open_issues_count = repoInfo.open_issues_count;
 
-	if (size !== repoPreviousState.size ||
+	if (updated_at !== repoPreviousState.updated_at ||
+		size !== repoPreviousState.size ||
 		open_issues_count !== repoPreviousState.open_issues_count) {
 		const embed = new discordEmbedBuilder()
 			.setColor('#0099ff')
@@ -70,6 +75,7 @@ async function checkForRepoChanges(repoOwner, repoName) {
 		}
 
 		repoPreviousState = {
+			updated_at: updated_at,
 			size: size,
 			open_issues_count: open_issues_count
 		};
